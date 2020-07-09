96dafddef89bab62549d5c3efa0405e4a97ead01
Suneet Tipirneni (Siris) <suneettipirneni@icloud.com>
2020-07-09 15:43:35 -0400

    Add the ampersand and bracket format for hex codes.

--- src/com/palmergames/bukkit/TownyChat/HexFormatter.java
+++ src/com/palmergames/bukkit/TownyChat/HexFormatter.java
@@ -8,13 +8,29 @@ import java.util.regex.Pattern;
 public class HexFormatter {
 
     public static final Pattern hexPattern = Pattern.compile("(?<!\\\\)(#[a-fA-F0-9]{6})");
+    public static final Pattern ampersandPattern = Pattern.compile("(?<!\\\\)(&#[a-fA-F0-9]{6})");
+    public static final Pattern bracketPattern = Pattern.compile("(?<!\\\\)\\{(#[a-fA-F0-9]{6})}");
 
     public static String translateHexColors(String str) {
-        Matcher hexMatcher = hexPattern.matcher(str);
+        final Matcher hexMatcher = hexPattern.matcher(str);
+        final Matcher ampMatcher = ampersandPattern.matcher(str);
+        final Matcher bracketMatcher = bracketPattern.matcher(str);
 
         while (hexMatcher.find()) {
             String hex = hexMatcher.group();
             str = str.replace(hex, ChatColor.of(hex).toString());
+        }
+
+        while (ampMatcher.find()) {
+            String hex = ampMatcher.group().replace("&", "");
+            str = str.replace(hex, ChatColor.of(hex).toString());
+            str = str.replace("&", "");
+        }
+
+        while (bracketMatcher.find()) {
+            String hex = bracketMatcher.group().replace("{", "").replace("}", "");
+            str = str.replace(hex, ChatColor.of(hex).toString());
+            str = str.replace("{", "").replace("}", "");
         }
 
         return str;
